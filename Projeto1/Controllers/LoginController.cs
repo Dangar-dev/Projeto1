f7273def7bbb73ce8f59052367a20b2dd9eb4a62
Dangar-dev <danielsobraldev@gmail.com>
2025-03-20 14:55:43 -0300

    Criando as informações do Login

--- Projeto1/Controllers/LoginController.cs
+++ Projeto1/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Projeto1.Views.Home.Repositorio;
 
 namespace Projeto1.Controllers
 {
@@ -8,5 +9,18 @@ namespace Projeto1.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Login(string email, string senha)
+        {
+            var usuario = _usuarioRepositorio.ObterUsuario(email);
+            if (usuario != null && usuario.Senha = senha)
+            {
+                //Autenticação bem-sucedida
+                return RedirectToAction("Index", "Home");
+            }
+            ModelState.AddModelError("", "Email ou senha inválidos.");
+            return View();
+        }
     }
 }
+
