dabdeb365f2a57f33f487d1198af20a6a4067d9e
Dangar-dev <danielsobraldev@gmail.com>
2025-03-20 15:13:21 -0300

    Adicionando as caracteristicas do Usuario

--- Projeto1/Controllers/LoginController.cs
+++ Projeto1/Controllers/LoginController.cs
@@ -1,10 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Projeto1.Models;
 using Projeto1.Views.Home.Repositorio;
 
 namespace Projeto1.Controllers
 {
     public class LoginController : Controller
     {
+        //Consutor
+        private readonly UsuarioRepositorio _usuarioRepositorio;
+
+        public LoginController(UsuarioRepositorio usuarioRepositorio)
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+        }
         public IActionResult Login()
         {
             return View();
@@ -20,6 +28,21 @@ namespace Projeto1.Controllers
             }
             ModelState.AddModelError("", "Email ou senha inválidos.");
             return View();
+        }
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Cadastro(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                _usuarioRepositorio.AdicionarUsuario(usuario);
+                return RedirectToAction("Login");
+
+            }
+            return View(usuario);
         }
     }
 }
